14c02e3200969397ddb44363a27e751771a5ac6a
karen okasaki <oks.karen@gmail.com>
2022-01-11 15:48:11 -0300

    operações adicionadas

--- calculator.js
+++ calculator.js
@@ -1,13 +1,40 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    a = 0
+    b = 0
+  }
+  if (!a){
+    a = 0 
+  }
+  if (!b) {
+    b = 0
+  }
+  
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    a = 0
+    b = 0
+  }
+  if (!a){
+    a = 0 
+  }
+  if (!b) {
+    b = 0
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if (a === 0) { 
+    throw console.error();
+  }
+  if (b === 0) {
+    throw console.error();
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
